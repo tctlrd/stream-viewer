5d7ed2c8d11f63bade5204b0c401a1401f98aeee
tctlrd <accts@xsxs.in>
2025-10-08 13:49:34 -0500

    split

--- split.sh
+++ split.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
-# Save as ~/.config/sway/arrange_grid.sh
 
-# Focus the workspace
-swaymsg "workspace 1"
+# Get all window IDs with app_id="mpv"
+windows=($(swaymsg -t get_tree | jq -r '.. | select(.app_id? == "mpv") | .id' | tr '\n' ' '))
 
-# Set layout to vertical split
-swaymsg "layout splitv"
-
-# Get window IDs
-windows=($(swaymsg -t get_tree | jq -r '.. | select(.app_id? == "mpv") | .id'))
-
-# First vertical split (top and bottom)
-if [ ${#windows[@]} -ge 2 ]; then
-    swaymsg "[con_id=${windows[0]}] split h"
-    swaymsg "[con_id=${windows[1]}] split h"
+# Check if we found any windows
+if [ ${#windows[@]} -eq 0 ]; then
+    echo "No MPV windows found!"
+    exit 1
 fi
 
-# Second vertical split (if we have 4 windows)
-if [ ${#windows[@]} -ge 4 ]; then
-    swaymsg "[con_id=${windows[0]}] focus"
-    swaymsg "focus down"
-    swaymsg "split h"
-    swaymsg "[con_id=${windows[2]}] split h"
-    swaymsg "[con_id=${windows[3]}] split h"
+echo "Found ${#windows[@]} MPV windows: ${windows[@]}"
+
+# Focus workspace 1 and set layout to splitv
+swaymsg "workspace 1; layout splitv"
+
+# For each window, focus it and split it
+for ((i=0; i<${#windows[@]}; i++)); do
+    if [ $i -gt 0 ]; then
+        # Split the container after the first window
+        swaymsg "[con_id=${windows[$i]}] focus; split h"
+    fi
+done
+
+# If we have more than 2 windows, create a second row
+if [ ${#windows[@]} -gt 2 ]; then
+    # Focus the first window and split down
+    swaymsg "[con_id=${windows[0]}] focus; splitv"
+    
+    # For remaining windows in the second row
+    for ((i=2; i<${#windows[@]}; i++)); do
+        swaymsg "[con_id=${windows[$i]}] focus; split h"
+    done
 fi
 
-# Make all windows equal size
-swaymsg "focus parent; split toggle split; focus parent; split toggle split"
+# Make all splits equal
+swaymsg "focus parent; toggle split; focus parent; toggle split"
